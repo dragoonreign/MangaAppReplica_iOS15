5062e96f2169a2d0a8c2399fd2d7b8cdb4949188
Junji Otsuka <jun531@gmail.com>
2024-10-03 19:52:39 -1000

    First Commit

---
+++ Project20(MangaAppReplica)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project20(MangaAppReplica)
+//
+//  Created by Jun on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project20(MangaAppReplica)/Project20_MangaAppReplica_App.swift
@@ -0,0 +1,17 @@
+//
+//  Project20_MangaAppReplica_App.swift
+//  Project20(MangaAppReplica)
+//
+//  Created by Jun on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Project20_MangaAppReplica_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
