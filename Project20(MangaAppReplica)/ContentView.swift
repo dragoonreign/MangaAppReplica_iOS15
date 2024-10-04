1f0c5cb5ddae929ef93eb363766bfd6c859876b5
Junji Otsuka <jun531@gmail.com>
2024-10-03 20:29:10 -1000

    Added example tabview and segmented picker

--- Project20(MangaAppReplica)/ContentView.swift
+++ Project20(MangaAppReplica)/ContentView.swift
@@ -8,9 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Flavor: String, CaseIterable, Identifiable {
+        case chocolate, vanilla, strawberry
+        var id: Self { self }
+    }
+    
+    @State private var selectedFlavor: Flavor = .chocolate
+
+    enum Topping: String, CaseIterable, Identifiable {
+        case nuts, cookies, blueberries
+        var id: Self { self }
+    }
+    
+    extension Flavor {
+        var suggestedTopping: Topping {
+            switch self {
+            case .chocolate: return .nuts
+            case .vanilla: return .cookies
+            case .strawberry: return .blueberries
+            }
+        }
+    }
+
+
+@State private var suggestedTopping: Topping = .nuts
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Picker("Flavor", selection: $selectedFlavor) {
+                    ForEach(Flavor.allCases) { flavor in
+                        Text(flavor.rawValue.capitalized)
+                    }
+                }
+                Picker("Topping", selection: $selectedTopping) {
+                    ForEach(Topping.allCases) { topping in
+                        Text(topping.rawValue.capitalized)
+                    }
+                }
+            }
+            .pickerStyle(.segmented)
+        }
+            
+        TabView {
+            Tab("Received", systemImage: "tray.and.arrow.down.fill") {
+                ReceivedView()
+            }
+            .badge(2)
+        
+        
+            Tab("Sent", systemImage: "tray.and.arrow.up.fill") {
+                SentView()
+            }
+        
+        
+            Tab("Account", systemImage: "person.crop.circle.fill") {
+                AccountView()
+            }
+            .badge("!")
+        }
     }
 }
 
