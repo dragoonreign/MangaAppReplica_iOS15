a253a7688b06be03f54ae8088da5c9cab1616a6d
Junji Otsuka <jun531@gmail.com>
2024-10-04 16:07:38 -1000

    Updated content view

--- Project20(MangaAppReplica)/ContentView.swift
+++ Project20(MangaAppReplica)/ContentView.swift
@@ -7,102 +7,113 @@
 
 import SwiftUI
 
-struct FreeMangaView: View {
-    VStack {
-        Picker("Flavor", selection: $selectedFlavor) {
-            ForEach(Flavor.allCases) { flavor in
-                Text(flavor.rawValue.capitalized)
-            }
-        }
-        Picker("Topping", selection: $selectedTopping) {
-            ForEach(Topping.allCases) { topping in
-                Text(topping.rawValue.capitalized)
-            }
+
+
+enum Topping: String, CaseIterable, Identifiable {
+    case nuts, cookies, blueberries
+    var id: Self { self }
+}
+
+enum Flavor: String, CaseIterable, Identifiable {
+    case chocolate, vanilla, strawberry
+    var id: Self { self }
+}
+
+extension Flavor {
+    var suggestedTopping: Topping {
+        switch self {
+        case .chocolate: return .nuts
+        case .vanilla: return .cookies
+        case .strawberry: return .blueberries
         }
     }
-    .pickerStyle(.segmented)
 }
 
 struct ContentView: View {
-    enum Flavor: String, CaseIterable, Identifiable {
-        case chocolate, vanilla, strawberry
-        var id: Self { self }
-    }
-    
     @State private var selectedFlavor: Flavor = .chocolate
-
-    enum Topping: String, CaseIterable, Identifiable {
-        case nuts, cookies, blueberries
-        var id: Self { self }
-    }
+    @State private var suggestedTopping: Topping = .nuts
     
-    extension Flavor {
-        var suggestedTopping: Topping {
-            switch self {
-            case .chocolate: return .nuts
-            case .vanilla: return .cookies
-            case .strawberry: return .blueberries
-            }
-        }
-    }
-
-
-@State private var suggestedTopping: Topping = .nuts
+    let manga = Manga()
+    
     var body: some View {
         NavigationView {
             // banner view here
             
             ScrollView(.vertical) {
+                // 新連載
+                Section {
+                    Picker("Flavor", selection: $selectedFlavor) {
+                        ForEach(Flavor.allCases) { flavor in
+                            Text(flavor.rawValue.capitalized)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
                 // all the manga here
                 // initially centered content
                 // ranks from 1 takes up the whole width 2-12 
                 // List(mangalist) // 2-12
                 
-                ScrollView(.horizontal) {
-                    // 新連載
-                }
-    
-                ScrollView(.horizontal) {
+                VStack {
                     // 最近見た
+                    Text("Recent")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.largeTitle)
+                    HorizontalMangaSelectionView(manga: manga)
                 }
-    
-                ScrollView(.horizontal) {
+                
+                VStack {
                     // ジャンプ+　無料作品
-                    //　same as the top scroll view
+                    Text("Jump+ Free Samples")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.largeTitle)
+                    HorizontalMangaSelectionView(manga: manga)
                 }
-    
-                ScrollView(.horizontal) {
+                
+                VStack {
                     // アニメ化作品
+                    Text("Recently Animated")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.largeTitle)
+                    HorizontalMangaSelectionView(manga: manga)
                 }
-    
-                ScrollView(.horizontal) {
+                
+                VStack {
                     // 最新の読切はこちら
+                    Text("Read one shots here")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.largeTitle)
+                    HorizontalMangaSelectionView(manga: manga)
                 }
-    
-                ScrollView(.horizontal) {
+                
+                VStack {
                     // 今だけ無料
+                    Text("Free for limited time")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .font(.largeTitle)
+                    HorizontalMangaSelectionView(manga: manga)
                 }
             }
-            .defaultScrollAnchor(.center)
-
+//            .defaultScrollAnchor(.center)
         }
             
         TabView {
-            Tab("Received", systemImage: "tray.and.arrow.down.fill") {
-                ReceivedView()
-            }
-            .badge(2)
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "tray.and.arrow.down.fill")
+                }
+                .badge(2)
         
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "tray.and.arrow.up.fill")
+                }
         
-            Tab("Sent", systemImage: "tray.and.arrow.up.fill") {
-                SentView()
-            }
-        
-        
-            Tab("Account", systemImage: "person.crop.circle.fill") {
-                AccountView()
-            }
-            .badge("!")
+            AccountView()
+                .tabItem {
+                    Label("Account", systemImage: "person.crop.circle.fill")
+                }
+                .badge("!")
         }
     }
 }
