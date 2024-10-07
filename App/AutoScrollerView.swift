607ed7da9e35c25c34950b3a423b4fe5ba16a5c2
Junji Otsuka <jun531@gmail.com>
2024-10-06 15:52:46 -1000

    Added AutoScroller

---
+++ App/AutoScrollerView.swift
@@ -0,0 +1,20 @@
+//
+//  AutoScrollerView.swift
+//  Project20(MangaAppReplica)
+//
+//  Created by Jun on 10/6/24.
+//
+
+import SwiftUI
+
+struct AutoScrollerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AutoScrollerView_Previews: PreviewProvider {
+    static var previews: some View {
+        AutoScrollerView()
+    }
+}
