0068201d92fdacb5a50de4f2c4f3b53256445c35
Junji Otsuka <jun531@gmail.com>
2024-10-07 18:59:55 -1000

    Updated UI in Veritcal

--- App/VerticalMangaSelectionView.swift
+++ App/VerticalMangaSelectionView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct VerticalMangaSelectionView: View {
+    @Environment(\.defaultMinListRowHeight) var minRowHeight
+    
     //get the list of manga here
     @EnvironmentObject var mangas: Mangas
     
@@ -32,38 +34,68 @@ struct VerticalMangaSelectionView: View {
         // 最近見た
         ScrollView(.vertical, showsIndicators: false) {
             VStack {
-                Text("Hello")
                 List(filteredMangas) { manga in
-                    Text("\(manga.author)")
+//                    Text("\(manga.author)")
+                    if (filter == .none) {
+                        Button() {
+                            mangas.toggleRecentlyRead(manga)
+                        } label: {
+                            // Show the image of the manga
+                            HStack {
+                                Image("US")
+//                                    .padding([.leading])
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: .infinity)
+                                    .frame(maxHeight: 50)
+                                
+                                VStack {
+                                    Text("\(manga.title)")
+//                                        .padding([.leading])
+                                    Text("\(manga.author)")
+//                                        .padding([.leading])
+                                }
+                                
+                                VStack {
+                                    Image(systemName: "eye")
+//                                        .padding([.leading])
+                                    Image(systemName: "message")
+//                                        .padding([.leading])
+                                }
+                                
+                                VStack {
+                                    Text("\(manga.totalViews)")
+//                                        .padding([.leading])
+                                    Text("\(manga.totalComments)")
+//                                        .padding([.leading])
+                                }
+                            }
+                        }
+                    } else if (filter == .recentlyRead) {
+                        Button() {
+                            print("recently read")
+                        } label: {
+                            // Show the image of the manga
+                            Image("UK")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: .infinity)
+                                .frame(maxHeight: 50)
+                        }
+                    } else if (filter == .favorites) {
+                        Button() {
+                            print("favorite")
+                        } label: {
+                            // Show the image of the manga
+                            Image("Italy")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: .infinity)
+                                .frame(maxHeight: 50)
+                        }
+                    }
                 }
-                Text("World")
-//                if (filter == .none) {
-//                    ForEach(filteredMangas) { manga in
-//                        Button() {
-//                            mangas.toggleRecentlyRead(manga)
-//                        } label: {
-//                            // Show the image of the manga
-//                            Image("US")
-//                                .resizable()
-//                                .scaledToFit()
-//                                .frame(maxWidth: .infinity)
-//                                .frame(maxHeight: 50)
-//                        }
-//                    }
-//                } else {
-//                    ForEach(filteredMangas) { manga in
-//                        Button() {
-//                            print("hello world")
-//                        } label: {
-//                            // Show the image of the manga
-//                            Image("UK")
-//                                .resizable()
-//                                .scaledToFit()
-//                                .frame(maxWidth: .infinity)
-//                                .frame(maxHeight: 50)
-//                        }
-//                    }
-//                }
+                .frame(minHeight: minRowHeight * CGFloat(filteredMangas.count + 2))
             }
         }
         
