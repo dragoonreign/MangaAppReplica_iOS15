6ef5c583f0654f46fb0546c67c38066c9de745e3
Junji Otsuka <jun531@gmail.com>
2024-10-11 08:32:13 -1000

    Update HomeView.swift

--- App/HomeView.swift
+++ App/HomeView.swift
@@ -12,30 +12,7 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var mangas: Mangas
     
-    @State var pickedDayOfWeek : Int {
-        var dayInt = 0
-        
-        switch(Date().formatted(Date.FormatStyle().weekday())) {
-        case("Sun"):
-            dayInt = 0
-        case("Mon"):
-            dayInt = 1
-        case("Tue"):
-            dayInt = 2
-        case("Wed"):
-            dayInt = 3
-        case("Thu"):
-            dayInt = 4
-        case("Fri"):
-            dayInt = 5
-        case("Sat"):
-            dayInt = 6
-        default:
-            dayInt = 0
-        }
-        
-        return dayInt
-    }
+    @State var pickedDayOfWeek = 1
     
 //    init() {
 //        navBarAppearance.configureWithOpaqueBackground()
