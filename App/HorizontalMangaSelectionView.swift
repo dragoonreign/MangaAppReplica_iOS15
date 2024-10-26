d45e8361d90f1e0bb11d9b13a1e84d7c2110f8b7
Junji Otsuka <jun531@gmail.com>
2024-10-26 09:21:59 -1000

    Update HorizontalMangaSelectionView.swift

--- App/HorizontalMangaSelectionView.swift
+++ App/HorizontalMangaSelectionView.swift
@@ -21,8 +21,6 @@ struct HorizontalMangaSelectionView: View {
     
     let filter: FilterType
     
-//    @State var sort: SortType = .name
-    
     var filteredMangas: [Manga] {
         switch filter {
         case .none:
@@ -35,15 +33,6 @@ struct HorizontalMangaSelectionView: View {
             return mangas.shelf.filter { $0.isUpdated }
         }
     }
-    
-//    var filteredSortedMangas: [Manga] {
-//        switch sort {
-//        case .updated:
-//            return filteredMangas.sorted { $0.isUpdated < $1.isUpdated }
-//        case .recent:
-//            return filteredMangas.sorted { $0.date < $1.date }
-//        }
-//    }
     
     var body: some View {
         // 最近見た
